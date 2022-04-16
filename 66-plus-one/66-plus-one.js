2dcaccc59cf63e53ed6ec2d8e818e29c27c815a0
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-04-16 14:43:09 +0900

    Time: 73 ms (62.47%), Space: 42.3 MB (37.91%) - LeetHub

---
+++ 66-plus-one/66-plus-one.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for (let i = 0; i < digits.length; i++) {
+      let num = BigInt(digits.join('')) + 1n;
+      return Array.from(num.toString()).join().split(',');
+      
+    }
+    
+};
