98bb5e1b3a30540b88f3529ae6c308242581bf72
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-04-16 17:04:02 +0900

    Time: 84 ms (64.27%), Space: 45 MB (36.12%) - LeetHub

---
+++ 169-majority-element/169-majority-element.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+  
+  for (let char of nums) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+};
