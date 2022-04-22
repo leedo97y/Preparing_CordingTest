28b521736422d2899bb42e6440e5658291c82ec1
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-04-23 00:04:20 +0900

    Time: 123 ms (67.69%), Space: 48.9 MB (44.58%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = nums[i] ** 2;
+  }
+  return nums.sort(function(a, b) {
+    return a - b;
+  })
+    
+};
