1f8f24dc72e8bb9e09c057010ff47d67002c8b34
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-04-22 23:25:08 +0900

    Time: 88 ms (45.63%), Space: 45.1 MB (65.04%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -8,7 +8,6 @@ var search = function(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     if (nums.includes(target) === true) {
       return nums.indexOf(target);
-      //console.log(nums.indexOf(target))
     } else {
       return -1;
     }
